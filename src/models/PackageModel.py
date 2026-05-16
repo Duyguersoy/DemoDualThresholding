6772d3a2498191c527bc7aaf07dbebe6a61ade0a
Duygu Ersoy <duyguersoy606@gmail.com>
2026-05-16 03:31:03 +0300

    seleksiyon

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -346,7 +346,7 @@ class DualThresholdingExecutor(Config):
         title = "Dual Thresholding"
         json_schema_extra = {
             "target": {
-                "value": 1
+                "value": 0
             }
         }
 
